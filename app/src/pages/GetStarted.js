0a3499b5e0073fd397168179d8905f9a76770c38
ifirmawan <firmawaneiwan@gmail.com>
2023-06-19 21:22:04 +0700

    [#10] Create 2 pages as examples

--- app/src/pages/GetStarted.js
+++ app/src/pages/GetStarted.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Text } from '@rneui/themed';
+import { Text, Button } from '@rneui/themed';
+
 const GetStarted = ({ navigation }) => {
   return (
     <View>
       <Text>Get started</Text>
+      <Button title="primary" onPress={() => navigation.navigate('Home')}>
+        Get Started
+      </Button>
     </View>
   );
 };
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,12 +1,63 @@
 import React from 'react';
-import { Text } from '@rneui/themed';
-import { DividerLayout } from '../components';
+import { Text, Header, Button } from '@rneui/themed';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { DividerLayout, Stack } from '../components';
 
 const Home = ({ navigation }) => {
+  const [search, setSearch] = React.useState(null);
   return (
-    <DividerLayout>
-      <Text>Home</Text>
-    </DividerLayout>
+    <Stack>
+      <Header
+        backgroundColor="#f3f4f6"
+        statusBarProps={{
+          backgroundColor: '#171717',
+        }}
+        containerStyle={{
+          height: 81,
+        }}
+      >
+        <Text>Hello</Text>
+        {''}
+        <Button type="clear">
+          <Icon name="ellipsis-vertical" />
+        </Button>
+      </Header>
+      <DividerLayout
+        search={{
+          callback: setSearch,
+          value: search,
+          placeholder: 'Find Form...',
+        }}
+        list={{
+          title: 'Sync recent form',
+          data: [
+            {
+              amount: 1,
+              description: 'Household Form 1',
+            },
+            {
+              amount: 2,
+              description: 'Household Form 1',
+            },
+            {
+              amount: 3,
+              description: 'Household Form 1',
+            },
+            {
+              amount: 4,
+              description: 'Household Form 1',
+            },
+            {
+              amount: 5,
+              description: 'Household Form 1',
+            },
+          ],
+        }}
+      >
+        <Text>Home</Text>
+        <Text>{search}</Text>
+      </DividerLayout>
+    </Stack>
   );
 };
 
