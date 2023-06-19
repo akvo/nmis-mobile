a99a39dd9dd24ee709b559340ba9b39223655f23
ifirmawan <firmawaneiwan@gmail.com>
2023-06-19 21:23:21 +0700

    [#10] Update unit test by remove input

--- app/__tests__/App.test.js
+++ app/__tests__/App.test.js
@@ -61,25 +61,4 @@ describe('App', () => {
       });
     });
   });
-
-  test('updates text value correctly', () => {
-    const { getByTestId } = render(<App />);
-    const input = getByTestId('inputText');
-    fireEvent.changeText(input, 'New Value');
-    expect(input.props.value).toBe('New Value');
-  });
-
-  test('updates number value correctly', () => {
-    const { getByTestId } = render(<App />);
-    const input = getByTestId('inputNumber');
-    fireEvent.changeText(input, '123');
-    expect(input.props.value).toBe('123');
-  });
-
-  test('keyboard type for number input', () => {
-    const { getByTestId } = render(<App />);
-    const input = getByTestId('inputNumber');
-
-    expect(input.props.keyboardType).toBe('numeric');
-  });
 });
