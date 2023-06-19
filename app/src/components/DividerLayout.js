253b96ea6448366aa1834e5b3431ab4788ccd626
ifirmawan <firmawaneiwan@gmail.com>
2023-06-19 21:21:04 +0700

    [#10] Create DividerLayout to split screen vertically

--- app/src/components/DividerLayout.js
+++ app/src/components/DividerLayout.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import { Text, SearchBar, Card } from '@rneui/themed';
+import Stack from './Stack';
+import Carousel from './Carousel';
 
-const DividerLayout = ({ children, searchBar = false, list = [], listTitle = null }) => {
+const DividerLayout = ({
+  children,
+  search = {
+    callback: null,
+    value: null,
+    placeholder: '',
+  },
+  list = {
+    title: null,
+    data: [],
+  },
+}) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.topSection}>Hello</View>
-      <View style={styles.bottomSection}>{children}</View>
-    </View>
+    <Stack>
+      <Stack>
+        {search?.callback && (
+          <SearchBar
+            placeholder={search?.placeholder}
+            onChangeText={search.callback}
+            value={search.value}
+          />
+        )}
+        {list?.title && <Text style={{ padding: 8, marginTop: 4 }}>{list.title}</Text>}
+        <Carousel control>
+          {list?.data?.map((d, dx) => {
+            return <Carousel.Summary key={dx} {...d} />;
+          })}
+        </Carousel>
+      </Stack>
+      <Stack background="#d1d5db">{children}</Stack>
+    </Stack>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-  },
-  topSection: {
-    flex: 1,
-    backgroundColor: 'rgb(229 231 235)',
-  },
-  bottomSection: {
-    flex: 1,
-    backgroundColor: 'rgb(209 213 219)',
-  },
-});
 export default DividerLayout;
