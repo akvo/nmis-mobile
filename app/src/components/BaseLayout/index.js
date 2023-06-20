6424e9595a175a54bac8ecf1b4a399807e59914c
ifirmawan <firmawaneiwan@gmail.com>
2023-06-20 19:59:12 +0700

    [#10] Update BaseLayout to handle search state

--- app/src/components/BaseLayout/index.js
+++ app/src/components/BaseLayout/index.js
@@ -11,12 +11,21 @@ const BaseLayout = ({
   search = {
     placeholder: null,
     show: false,
+    value: null,
+    action: null,
   },
 }) => {
+  let searchProps = {
+    placeholder: search?.placeholder,
+    value: search?.value,
+  };
+  if (search?.action && typeof search.action === 'function') {
+    searchProps = { ...searchProps, onChangeText: search.action };
+  }
   return (
     <Stack>
       {title && <PageTitle text={title} back={back} />}
-      {search.show && <SearchBar placeholder={search?.placeholder} testID="search-bar" />}
+      {search.show && <SearchBar {...searchProps} testID="search-bar" />}
       {children}
     </Stack>
   );
