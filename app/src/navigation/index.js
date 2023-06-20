ca8b474c5ba6157234d8eb8967a8aaa22edb9e1e
ifirmawan <firmawaneiwan@gmail.com>
2023-06-20 18:37:39 +0700

    [#10] Update routes to include formData & formAction

--- app/src/navigation/index.js
+++ app/src/navigation/index.js
@@ -1,10 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { GetStartedPage, HomePage } from '../pages';
+import { HomePage, FormActionPage, FormDataPage } from '../pages';
 
-export default function Navigation({ colorScheme }) {
+export default function Navigation(props) {
   return (
-    <NavigationContainer>
+    <NavigationContainer {...props}>
       <RootNavigator />
     </NavigationContainer>
   );
@@ -15,8 +15,9 @@ const Stack = createNativeStackNavigator();
 function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="GetStarted" component={GetStartedPage} />
       <Stack.Screen name="Home" component={HomePage} />
+      <Stack.Screen name="FormAction" component={FormActionPage} />
+      <Stack.Screen name="FormData" component={FormDataPage} />
     </Stack.Navigator>
   );
 }
