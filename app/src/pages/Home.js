7947f2c0def76311db1cc6a6067daf477abc26e2
ifirmawan <firmawaneiwan@gmail.com>
2023-06-20 18:36:45 +0700

    [#10] Implement BaseLayout at Homepage

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,63 +1,27 @@
 import React from 'react';
-import { Text, Header, Button } from '@rneui/themed';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { DividerLayout, Stack } from '../components';
+import { BaseLayout } from '../components';
 
 const Home = ({ navigation }) => {
-  const [search, setSearch] = React.useState(null);
+  const goToFormAction = (id) => {
+    const findData = data?.find((d) => d?.id === id);
+    navigation.navigate('FormAction', { id, name: findData?.name });
+  };
+
+  const data = Array.from({ length: 100 }).map((_, dx) => ({
+    id: dx,
+    name: `Household ${dx + 1}`,
+    subtitles: ['Submitted: 20', 'Draft: 1', 'Synced: 11'],
+  }));
   return (
-    <Stack>
-      <Header
-        backgroundColor="#f3f4f6"
-        statusBarProps={{
-          backgroundColor: '#171717',
-        }}
-        containerStyle={{
-          height: 81,
-        }}
-      >
-        <Text>Hello</Text>
-        {''}
-        <Button type="clear">
-          <Icon name="ellipsis-vertical" />
-        </Button>
-      </Header>
-      <DividerLayout
-        search={{
-          callback: setSearch,
-          value: search,
-          placeholder: 'Find Form...',
-        }}
-        list={{
-          title: 'Sync recent form',
-          data: [
-            {
-              amount: 1,
-              description: 'Household Form 1',
-            },
-            {
-              amount: 2,
-              description: 'Household Form 1',
-            },
-            {
-              amount: 3,
-              description: 'Household Form 1',
-            },
-            {
-              amount: 4,
-              description: 'Household Form 1',
-            },
-            {
-              amount: 5,
-              description: 'Household Form 1',
-            },
-          ],
-        }}
-      >
-        <Text>Home</Text>
-        <Text>{search}</Text>
-      </DividerLayout>
-    </Stack>
+    <BaseLayout
+      title="Form Lists"
+      search={{
+        show: true,
+        placeholder: 'Search form',
+      }}
+    >
+      <BaseLayout.Content data={data} action={goToFormAction} columns={2} />
+    </BaseLayout>
   );
 };
 
