dc52931a2976ca83e88a54b5b2d5d892c97a99b9
wayangalihpratama <wgprtm@gmail.com>
2023-06-21 17:09:33 +0800

    [#13] Fix scroll view

--- app/src/form/Webform.js
+++ app/src/form/Webform.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BaseLayout } from '../components';
-import { ScrollView } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { Text, Input, CheckBox, Button } from '@rneui/themed';
 import { Formik } from 'formik';
 import { styles } from './styles';
@@ -18,93 +18,95 @@ const Webform = ({ navigation, route }) => {
 
   return (
     <BaseLayout title={route?.params?.name} back={goBack}>
-      <BaseLayout.Content>
-        <Formik initialValues={{}} onSubmit={(values) => console.log(values)}>
-          {({ handleChange, handleSubmit, setFieldValue, values }) => (
-            <ScrollView>
-              {/* Text */}
-              <>
-                <FieldLabel label="Name" />
-                <Input onChangeText={handleChange('name')} value={values.name} />
-              </>
-              {/* DatePicker */}
-              <>
-                <FieldLabel label="Birth Date" />
-                <Input
-                  onPressIn={() => setShowDatePicker(true)}
-                  showSoftInputOnFocus={false}
-                  value={values.birthDate?.toLocaleDateString()}
-                />
-                {showDatepicker && (
-                  <DateTimePicker
-                    testID="dateTimePicker"
-                    value={values.birthDate || now}
-                    mode="date"
-                    onChange={({ nativeEvent: val }) => {
-                      setShowDatePicker(false);
-                      setFieldValue('birthDate', new Date(val.timestamp));
-                    }}
+      <ScrollView>
+        <BaseLayout.Content>
+          <Formik initialValues={{}} onSubmit={(values) => console.log(values)}>
+            {({ handleChange, handleSubmit, setFieldValue, values }) => (
+              <View style={styles.formContainer}>
+                {/* Text */}
+                <>
+                  <FieldLabel label="Name" />
+                  <Input onChangeText={handleChange('name')} value={values.name} />
+                </>
+                {/* DatePicker */}
+                <>
+                  <FieldLabel label="Birth Date" />
+                  <Input
+                    onPressIn={() => setShowDatePicker(true)}
+                    showSoftInputOnFocus={false}
+                    value={values.birthDate?.toLocaleDateString()}
                   />
-                )}
-              </>
-              {/* Number */}
-              <>
-                <FieldLabel label="Age" />
-                <Input
-                  keyboardType="numeric"
-                  onChangeText={handleChange('age')}
-                  value={values.age}
-                />
-              </>
-              {/* Radio */}
-              <>
-                <FieldLabel label="Gender" />
-                <CheckBox
-                  checked={values.gender === 'male'}
-                  onPress={() => setFieldValue('gender', 'male')}
-                  title="Male"
-                  checkedIcon="dot-circle-o"
-                  uncheckedIcon="circle-o"
-                />
-                <CheckBox
-                  checked={values.gender === 'female'}
-                  onPress={() => setFieldValue('gender', 'female')}
-                  title="Female"
-                  checkedIcon="dot-circle-o"
-                  uncheckedIcon="circle-o"
-                />
-              </>
-              {/* Checkbox */}
-              <>
-                <FieldLabel label="Hobby" />
-                {['Reading', 'Traveling', 'Programming'].map((val, ival) => (
+                  {showDatepicker && (
+                    <DateTimePicker
+                      testID="dateTimePicker"
+                      value={values.birthDate || now}
+                      mode="date"
+                      onChange={({ nativeEvent: val }) => {
+                        setShowDatePicker(false);
+                        setFieldValue('birthDate', new Date(val.timestamp));
+                      }}
+                    />
+                  )}
+                </>
+                {/* Number */}
+                <>
+                  <FieldLabel label="Age" />
+                  <Input
+                    keyboardType="numeric"
+                    onChangeText={handleChange('age')}
+                    value={values.age}
+                  />
+                </>
+                {/* Radio */}
+                <>
+                  <FieldLabel label="Gender" />
                   <CheckBox
-                    key={ival}
-                    checked={values.hobby?.[ival] === val}
-                    onPress={() => {
-                      values.hobby?.[ival] === val
-                        ? setFieldValue(`hobby.${ival}`, null)
-                        : setFieldValue(`hobby.${ival}`, val);
-                    }}
-                    title={val}
+                    checked={values.gender === 'male'}
+                    onPress={() => setFieldValue('gender', 'male')}
+                    title="Male"
+                    checkedIcon="dot-circle-o"
+                    uncheckedIcon="circle-o"
                   />
-                ))}
-              </>
-              {/* TextArea */}
-              <>
-                <FieldLabel label="Comment" />
-                <Input
-                  multiline={true}
-                  numberOfLines={4}
-                  onChangeText={handleChange('comment')}
-                  value={values.comment}
-                />
-              </>
-              <Button onPress={handleSubmit} title="Submit" />
-            </ScrollView>
-          )}
-        </Formik>
-      </BaseLayout.Content>
+                  <CheckBox
+                    checked={values.gender === 'female'}
+                    onPress={() => setFieldValue('gender', 'female')}
+                    title="Female"
+                    checkedIcon="dot-circle-o"
+                    uncheckedIcon="circle-o"
+                  />
+                </>
+                {/* Checkbox */}
+                <>
+                  <FieldLabel label="Hobby" />
+                  {['Reading', 'Traveling', 'Programming'].map((val, ival) => (
+                    <CheckBox
+                      key={ival}
+                      checked={values.hobby?.[ival] === val}
+                      onPress={() => {
+                        values.hobby?.[ival] === val
+                          ? setFieldValue(`hobby.${ival}`, null)
+                          : setFieldValue(`hobby.${ival}`, val);
+                      }}
+                      title={val}
+                    />
+                  ))}
+                </>
+                {/* TextArea */}
+                <>
+                  <FieldLabel label="Comment" />
+                  <Input
+                    multiline={true}
+                    numberOfLines={4}
+                    onChangeText={handleChange('comment')}
+                    value={values.comment}
+                  />
+                </>
+                <Button onPress={handleSubmit} title="Submit" />
+              </View>
+            )}
+          </Formik>
+        </BaseLayout.Content>
+      </ScrollView>
     </BaseLayout>
   );
 };
--- app/src/form/styles.js
+++ app/src/form/styles.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  formContainer: {
+    flex: 1,
+  },
   fieldLabel: {
     marginTop: 10,
     marginLeft: 10,
