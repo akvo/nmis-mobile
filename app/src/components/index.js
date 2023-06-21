a83c25702b55b9e7a8c000760fa340b405711210
ifirmawan <firmawaneiwan@gmail.com>
2023-06-21 14:52:58 +0700

    [#12] Create own Imaga & export it in components

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Stack } from './Stack';
 export { default as BaseLayout } from './BaseLayout';
 export { default as Card } from './Card';
+export { default as Image } from './Image';
+export { default as CenterLayout } from './CenterLayout';
