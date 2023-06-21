df014dfc905d00c463a541b8fdd6f5fea6d5205e
ifirmawan <firmawaneiwan@gmail.com>
2023-06-21 14:58:28 +0700

    [#12] Implement CenterLayout in GetStarted

--- app/src/pages/GetStarted.js
+++ app/src/pages/GetStarted.js
@@ -1,34 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Text, Button } from '@rneui/themed';
+import { CenterLayout, Image } from '../components';
 
 const GetStarted = ({ navigation }) => {
   const goToLogin = () => {
-    navigation.navigate('Home');
+    navigation.navigate('AuthForm');
   };
+  const titles = ['Get Started', 'collecting data the', 'smart way'];
   return (
-    <View
-      style={{
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingHorizontal: 16,
-        gap: 36,
-      }}
-    >
-      <View style={{ width: 120, height: 120, backgroundColor: '#e5e7eb', borderRadius: 4 }} />
-      <View style={{ display: 'flex', alignItems: 'center' }}>
-        <Text h4>Get Started</Text>
-        <Text h4>collecting data the</Text>
-        <Text h4>smart way</Text>
-      </View>
+    <CenterLayout title={titles}>
+      <Image />
+      <CenterLayout.Titles items={titles} />
       <Text>Lorem Ipsum dolor sit amet dolor random</Text>
       <Button title="primary" onPress={goToLogin}>
         Get Started
       </Button>
-    </View>
+    </CenterLayout>
   );
 };
 
