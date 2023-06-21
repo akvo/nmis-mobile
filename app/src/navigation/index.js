29105b6d65da486f2bd4a729ebd21dcd58f527e1
ifirmawan <firmawaneiwan@gmail.com>
2023-06-21 14:58:49 +0700

    [#12] Create new page: AuthForm

--- app/src/navigation/index.js
+++ app/src/navigation/index.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomePage, FormActionPage, FormDataPage, GetStartedPage } from '../pages';
+import { HomePage, FormActionPage, FormDataPage, GetStartedPage, AuthFormPage } from '../pages';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +8,7 @@ const RootNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="GetStarted" component={GetStartedPage} />
+      <Stack.Screen name="AuthForm" component={AuthFormPage} />
       <Stack.Screen name="Home" component={HomePage} />
       <Stack.Screen name="FormAction" component={FormActionPage} />
       <Stack.Screen name="FormData" component={FormDataPage} />
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as HomePage } from './Home';
 export { default as FormActionPage } from './FormAction';
 export { default as FormDataPage } from './FormData';
 export { default as GetStartedPage } from './GetStarted';
+export { default as AuthFormPage } from './AuthForm';
