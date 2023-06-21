9503bc87f0707ac733b0acf695ee65634baef047
dedenbangkit <mail@dedenbangkit.com>
2023-06-21 20:57:05 +0700

    [#16] Sync API Endpoint

--- backend/php/assignment.php
+++ backend/php/assignment.php
@@ -1,23 +1,24 @@
 <?php
-    header('Content-Type: application/json');
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['password']) && $_POST['password'] === 'testing') {
-            http_response_code(200);
-						echo json_encode(array(
-							'message' => 'Success',
-							'forms' => array(
-									'/forms/519630048',
-									'/forms/533560002',
-									'/forms/563350033',
-									'/forms/567490004',
-									'/forms/603050002',
-							),
-							'token' => '1234567890',
-						));
-        } else {
-            http_response_code(403);
-        }
+header('Content-Type: application/json');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['code']) && $_POST['code'] === 'testing123') {
+        http_response_code(200);
+        echo json_encode(array(
+            'message' => 'Success',
+            'formsUrl' => array(
+                '/forms/519630048',
+                '/forms/533560002',
+                '/forms/563350033',
+                '/forms/567490004',
+                '/forms/603050002',
+            ),
+            'syncToken' => 'Bearer eyjtoken',
+        ));
     } else {
-        http_response_code(405);
+        http_response_code(403);
+        echo json_encode(["error" => "Invalid credentials"]);
     }
+} else {
+    http_response_code(405);
+}
 ?>
