1abbff18c2ea90bbf63635fd33922f9eb49d5f46
ifirmawan <firmawaneiwan@gmail.com>
2023-06-22 17:03:32 +0700

    [#15] Load create tables in App.js

--- app/App.js
+++ app/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Navigation from './src/navigation';
+import { conn, initialQuery } from './src/database';
+
+const db = conn.init;
 
 const App = () => {
+  React.useEffect(() => {
+    conn.tx(db, initialQuery());
+  }, []);
   return (
     <SafeAreaProvider>
       <Navigation testID="navigation-element" />
