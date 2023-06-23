b926bea2433ef236571d2cd6fb75cddcbcba74a1
dedenbangkit <mail@dedenbangkit.com>
2023-06-23 10:35:25 +0700

    [#20] Modify Update version CLI

--- backend/update-version.sh
+++ backend/update-version.sh
@@ -9,6 +9,5 @@ fi
 
 for file in ./backend/json/*.json; do
     docker run -v "${PWD}":/data -w /data stedolan/jq --arg fv "$form_version" '.version = $fv' "$file" >temp.json && mv temp.json "$file"
-    docker run -v "${PWD}":/data -w /data stedolan/jq 'del(.question_group[].question[].api)' "$file" >temp.json && mv temp.json "$file"
     echo "updated $file"
 done
