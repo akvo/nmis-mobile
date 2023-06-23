393935b79c567405df88d626bdb20438dd5b8ab1
ifirmawan <firmawaneiwan@gmail.com>
2023-06-23 21:59:58 +0700

    [#21] Mock netInfo in App test

--- app/__tests__/App.test.js
+++ app/__tests__/App.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-native-testing-library';
+import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
+jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
+
 import App from '../App';
 
 describe('App', () => {
