6f571cd19f1bd8552aec4f67a1da45092f6f32fb
ifirmawan <firmawaneiwan@gmail.com>
2023-06-23 13:58:04 +0700

    [#15] Replace initialQuery w/ query.initialQuery

--- app/App.js
+++ app/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Navigation from './src/navigation';
-import { conn, initialQuery } from './src/database';
+import { conn, query, tables } from './src/database';
 
 const db = conn.init;
 
 const App = () => {
   React.useEffect(() => {
-    conn.tx(db, initialQuery());
+    const queries = tables.map((t) => query.initialQuery(t.name, t.fields)).join(' ');
+    conn.tx(db, queries).then((res) => {
+      console.log('results', res);
+    });
   }, []);
   return (
     <SafeAreaProvider>
