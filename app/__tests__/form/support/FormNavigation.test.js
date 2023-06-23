7eb6f2212dabaf52facd7a97e0a37ea16612a1ed
wayangalihpratama <wgprtm@gmail.com>
2023-06-23 10:54:24 +0800

    [#13] Update form navigation test

--- app/__tests__/form/support/FormNavigation.test.js
+++ app/__tests__/form/support/FormNavigation.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { render } from 'react-native-testing-library';
+import { render, fireEvent } from 'react-native-testing-library';
 import { FormNavigation } from '../../../src/form/support';
 
 describe('FormNavigation component', () => {
   it('renders form navigation correctly', async () => {
     await new Promise((resolve) => setTimeout(resolve, 100));
 
-    const { getByTestId, getByText } = render(
+    const setActiveGroup = jest.fn();
+    const onSubmit = jest.fn();
+
+    const { getByTestId, getByText, queryByTestId } = render(
       <FormNavigation
         activeGroup={0}
-        setActiveGroup={() => {}}
-        onSubmit={() => {}}
+        setActiveGroup={setActiveGroup}
+        onSubmit={onSubmit}
         totalGroup={2}
       />,
     );
@@ -24,28 +27,42 @@ describe('FormNavigation component', () => {
 
     const btnNext = getByTestId('form-nav-btn-next');
     expect(btnNext).toBeDefined();
+
+    const btnSubmit = queryByTestId('form-btn-submit');
+    expect(btnSubmit).toBeNull();
   });
 
-  it('renders last group in form navigation correctly', async () => {
-    await new Promise((resolve) => setTimeout(resolve, 100));
+  test('clicking Next should increment activeGroup if not on the last group', () => {
+    const setActiveGroup = jest.fn();
+    const onSubmit = jest.fn();
 
-    const { getByTestId, getByText } = render(
-      <FormNavigation
-        activeGroup={1}
-        setActiveGroup={() => {}}
-        onSubmit={() => {}}
-        totalGroup={2}
-      />,
+    const { getByTestId } = render(
+      <FormNavigation activeGroup={0} setActiveGroup={setActiveGroup} onSubmit={onSubmit} />,
     );
 
-    const btnBack = getByTestId('form-nav-btn-back');
-    expect(btnBack).toBeDefined();
+    const btnNext = getByTestId('form-nav-btn-next');
+    fireEvent.press(btnNext);
+    expect(setActiveGroup).toHaveBeenCalledWith(1);
+  });
+
+  test('clicking Submit should call onSubmit if on the last group', () => {
+    const setActiveGroup = jest.fn();
+    const onSubmit = jest.fn();
+
+    const { getByTestId, getByText, queryByTestId } = render(
+      <FormNavigation activeGroup={1} setActiveGroup={setActiveGroup} onSubmit={onSubmit} />,
+    );
 
     const groupCounter = getByTestId('form-nav-group-count');
     expect(groupCounter).toBeDefined();
     expect(getByText('2/2')).toBeDefined();
 
-    const btnNSubmit = getByTestId('form-btn-submit');
-    expect(btnNSubmit).toBeDefined();
+    const btnNext = queryByTestId('form-nav-btn-next');
+    expect(btnNext).toBeNull();
+
+    const btnSubmit = getByTestId('form-btn-submit');
+    expect(btnSubmit).toBeDefined();
+    fireEvent.press(btnSubmit);
+    expect(onSubmit).toHaveBeenCalled();
   });
 });
