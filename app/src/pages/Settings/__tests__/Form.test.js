455f31c0a6ed14e74520d42411ecb7d7814aff46
ifirmawan <firmawaneiwan@gmail.com>
2023-06-28 11:44:32 +0700

    [#23] Update unit test: SettingsForm

--- app/src/pages/Settings/__tests__/Form.test.js
+++ app/src/pages/Settings/__tests__/Form.test.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import { render, waitFor } from 'react-native-testing-library';
+import { renderHook, fireEvent } from '@testing-library/react-native';
 import { route, MockNavigationProvider } from '@react-navigation/native';
 import SettingsForm from '../Form';
+import { config } from '../config';
+import { UIState } from '../../../store';
 
 jest.mock('@react-navigation/native');
 
 describe('SettingsForm', () => {
   it('renders correctly', async () => {
-    route.params = { id: 1, name: 'User Interface' };
-    const { unmount } = await waitFor(() =>
+    const params = { id: 2, name: 'User Interface' };
+    route.params = params;
+    const findConfig = config.find((c) => c?.id === params.id);
+
+    const { unmount, getByText, getByTestId } = await waitFor(() =>
       render(
         <MockNavigationProvider>
-          <SettingsForm />
+          <SettingsForm route={route} />
         </MockNavigationProvider>,
       ),
     );
+
+    const isDarkModeEl = getByTestId('settings-form-switch-1');
+    expect(isDarkModeEl).toBeDefined();
+
+    findConfig?.fields?.forEach((f) => {
+      const labelEl = getByText(f.label);
+      expect(labelEl).toBeDefined();
+    });
+
+    const langItem = getByTestId('settings-form-item-0');
+    fireEvent.press(langItem);
+    const dialogEl = getByTestId('settings-form-dialog');
+    expect(dialogEl).toBeDefined();
 
     unmount();
   });
