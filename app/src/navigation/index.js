81576fd5010f835f86d8ae18594268e34268b086
ifirmawan <firmawaneiwan@gmail.com>
2023-06-29 16:31:19 +0700

    [#23] Change all FormAction to ManageForm

--- app/src/navigation/index.js
+++ app/src/navigation/index.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
   HomePage,
-  FormActionPage,
+  ManageFormPage,
   FormDataPage,
   GetStartedPage,
   AuthFormPage,
@@ -19,7 +19,7 @@ const RootNavigator = () => {
       <Stack.Screen name="GetStarted" component={GetStartedPage} />
       <Stack.Screen name="AuthForm" component={AuthFormPage} />
       <Stack.Screen name="Home" component={HomePage} />
-      <Stack.Screen name="FormAction" component={FormActionPage} />
+      <Stack.Screen name="ManageForm" component={ManageFormPage} />
       <Stack.Screen name="FormData" component={FormDataPage} />
       <Stack.Screen name="Settings" component={SettingsPage} />
       <Stack.Screen name="SettingsForm" component={SettingsFormPage} />
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as HomePage } from './Home';
-export { default as FormActionPage } from './FormAction';
+export { default as ManageFormPage } from './ManageForm';
 export { default as FormDataPage } from './FormData';
 export { default as GetStartedPage } from './GetStarted';
 export { default as AuthFormPage } from './AuthForm';
