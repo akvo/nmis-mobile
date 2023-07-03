62ca1923de535f7a4b99e7e0f9ff10c655d7f999
wayangalihpratama <wgprtm@gmail.com>
2023-07-03 16:26:01 +0800

    [#32] Add sync form check on home/form list page

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import { BaseLayout } from '../components';
+import { api } from '../lib';
+import { AuthState, UIState } from '../store';
+import { crudForms } from '../database/crud';
 
 const Home = ({ navigation }) => {
+  const isOnline = UIState.useState((s) => s.online);
+  const authenticationCode = AuthState.useState((s) => s.authenticationCode);
   const [search, setSearch] = React.useState(null);
+
   const goToManageForm = (id) => {
     const findData = data?.find((d) => d?.id === id);
     navigation.navigate('ManageForm', { id, name: findData?.name });
   };
+
+  React.useEffect(() => {
+    // check forms new form version available
+    if (!isOnline || !authenticationCode || authenticationCode === '') {
+      return;
+    }
+    const data = new FormData();
+    data.append('code', authenticationCode);
+    api
+      .post('/auth', data, { headers: { 'Content-Type': 'multipart/form-data' } })
+      .then(async (res) => {
+        const { data } = res;
+        data.formsUrl.forEach(async (form) => {
+          const formExist = await crudForms.selectFormByIdAndVersion({ ...form });
+          if (formExist) {
+            // skip
+            return;
+          }
+          const formRes = await api.get(form.url);
+          // update previous form latest value to 0
+          const updatedForm = await crudForms.updateForm({ ...form });
+          console.info('Updated Forms...', form.id, updatedForm);
+          const savedForm = await crudForms.addForm({ ...form, formJSON: formRes?.data });
+          console.info('Saved Forms...', form.id, savedForm);
+        });
+      });
+  }, []);
 
   const data = Array.from({ length: 100 })
     .map((_, dx) => ({
