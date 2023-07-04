65aacc63e3d05ef653cf634b12ff81b4fe9d8039
wayangalihpratama <wgprtm@gmail.com>
2023-07-04 14:16:11 +0800

    [#32] Try to fetch forms in bg task

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -2,7 +2,77 @@ import React from 'react';
 import { BaseLayout } from '../components';
 import { api } from '../lib';
 import { AuthState, UIState, FormState } from '../store';
-import { crudForms } from '../database/crud';
+import { crudForms, crudSessions } from '../database/crud';
+// import { bgTask } from '../lib';
+import * as TaskManager from 'expo-task-manager';
+import * as BackgroundFetch from 'expo-background-fetch';
+
+const TASK_NAME = 'sync-form-version';
+
+const syncFormVersion = async () => {
+  try {
+    // find last session
+    const session = await crudSessions.selectLastSession();
+    console.log('[bgTask]', session);
+    if (!session) {
+      return;
+    }
+    const authenticationCode = session.passcode;
+    const data = new FormData();
+    data.append('code', authenticationCode);
+    api
+      .post('/auth', data, { headers: { 'Content-Type': 'multipart/form-data' } })
+      .then(async (res) => {
+        const { data } = res;
+        data.formsUrl.forEach(async (form) => {
+          const formExist = await crudForms.selectFormByIdAndVersion({ ...form });
+          if (formExist) {
+            // skip
+            console.info('[bgTask]Form exist:', form.id);
+            return;
+          }
+          const formRes = await api.get(form.url);
+          // update previous form latest value to 0
+          const updatedForm = await crudForms.updateForm({ ...form });
+          console.info('[bgTask]Updated Forms...', form.id, updatedForm);
+          const savedForm = await crudForms.addForm({ ...form, formJSON: formRes?.data });
+          console.info('[bgTask]Saved Forms...', form.id, savedForm);
+        });
+      });
+  } catch (err) {
+    console.error('sycnFormVersion failed:', err);
+  }
+};
+
+TaskManager.defineTask(TASK_NAME, async () => {
+  try {
+    syncFormVersion();
+    return BackgroundFetch.BackgroundFetchResult.NewData;
+  } catch (err) {
+    console.error('define task manager failed:', err);
+    return BackgroundFetch.Result.Failed;
+  }
+});
+
+const registerBackgroundTask = async () => {
+  try {
+    await BackgroundFetch.registerTaskAsync(TASK_NAME, {
+      minimumInterval: 0.5 * 60, // in minutes
+      stopOnTerminate: false, // android only,
+      startOnBoot: true, // android only
+    });
+  } catch (err) {
+    console.error('Task Register failed:', err);
+  }
+};
+
+const unregisterBackgroundTask = async () => {
+  try {
+    await BackgroundFetch.unregisterTaskAsync(TASK_NAME);
+  } catch (err) {
+    console.error('Task Unregister failed:', err);
+  }
+};
 
 const Home = ({ navigation }) => {
   const isOnline = UIState.useState((s) => s.online);
@@ -20,31 +90,18 @@ const Home = ({ navigation }) => {
     }, 100);
   };
 
-  React.useEffect(() => {
-    // check forms new form version available
-    if (!isOnline || !authenticationCode || authenticationCode === '') {
-      return;
+  const checkStatusAsync = async () => {
+    const status = await BackgroundFetch.getStatusAsync();
+    const isRegistered = await TaskManager.isTaskRegisteredAsync(TASK_NAME);
+    console.log('BackgroundTask Status', status, isRegistered);
+    if (BackgroundFetch.BackgroundFetchStatus?.[status] === 'Available' && !isRegistered) {
+      await registerBackgroundTask();
     }
-    const data = new FormData();
-    data.append('code', authenticationCode);
-    api
-      .post('/auth', data, { headers: { 'Content-Type': 'multipart/form-data' } })
-      .then(async (res) => {
-        const { data } = res;
-        data.formsUrl.forEach(async (form) => {
-          const formExist = await crudForms.selectFormByIdAndVersion({ ...form });
-          if (formExist) {
-            // skip
-            return;
-          }
-          const formRes = await api.get(form.url);
-          // update previous form latest value to 0
-          const updatedForm = await crudForms.updateForm({ ...form });
-          console.info('Updated Forms...', form.id, updatedForm);
-          const savedForm = await crudForms.addForm({ ...form, formJSON: formRes?.data });
-          console.info('Saved Forms...', form.id, savedForm);
-        });
-      });
+    console.log('BackgroundTask Status', status, isRegistered);
+  };
+
+  React.useEffect(() => {
+    checkStatusAsync();
   }, []);
 
   React.useState(() => {
