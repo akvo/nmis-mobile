0ea9b63058d9f76021d8a8066ca153f2e8121fb7
ifirmawan <firmawaneiwan@gmail.com>
2023-07-04 16:33:54 +0700

    [#31] Create unit test: loc library

---
+++ app/src/lib/__test__/loc.test.js
@@ -0,0 +1,28 @@
+import { Location } from 'expo-location';
+import loc from '../loc';
+
+jest.mock('expo-location');
+
+describe('lib/loc.js', () => {
+  test('get current location successfully', () => {
+    loc.getCurrentLocation((res) => {
+      expect(res).toEqual({
+        coords: {
+          latitude: 37.12345,
+          longitude: -122.6789,
+        },
+      });
+    });
+  });
+
+  it('should handle denied permission', () => {
+    const mockRequestDenied = jest.fn().mockImplementation(() => {
+      return Promise.resolve({ status: 'denied' });
+    });
+    Location.requestForegroundPermissionsAsync.mockImplementation(mockRequestDenied);
+
+    loc.getCurrentLocation(null, (err) => {
+      expect(err.message).toEqual('Permission to access location was denied');
+    });
+  });
+});
