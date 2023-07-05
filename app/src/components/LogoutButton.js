02d5944e9fd3e1671844432c149d0b39ba41a6a0
ifirmawan <firmawaneiwan@gmail.com>
2023-07-05 11:38:34 +0700

    [#35] Clear state & session on successfull logout

--- app/src/components/LogoutButton.js
+++ app/src/components/LogoutButton.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Platform, ToastAndroid } from 'react-native';
 import { ListItem, Dialog, Text, Icon } from '@rneui/themed';
+import { useNavigation } from '@react-navigation/native';
+import { AuthState } from '../store';
+import { conn, query } from '../database';
+
+const db = conn.init;
 
 const LogoutButton = () => {
-  const [visible, setVisible] = React.useState(false);
+  const [visible, setVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
 
   const handleNoPress = () => {
     setVisible(false);
   };
 
+  const handleError = () => {
+    if (Platform.OS === 'android') {
+      ToastAndroid.show('Logout Failed: Please try again later', ToastAndroid.LONG);
+    }
+  };
+
   const handleYesPress = () => {
-    setVisible(false);
+    const table = 'sessions';
+    const clearQuery = query.clear(table);
+    console.log('clear', clearQuery);
+    setLoading(true);
+    conn
+      .tx(db, clearQuery)
+      .then(() => {
+        AuthState.update((s) => {
+          s.token = null;
+        });
+        navigation.navigate('GetStarted');
+        setLoading(false);
+        setVisible(false);
+      })
+      .catch(() => {
+        handleError();
+        setLoading(false);
+        setVisible(false);
+      });
   };
 
   return (
@@ -21,11 +52,15 @@ const LogoutButton = () => {
         </ListItem.Content>
         <Icon name="log-out-outline" type="ionicon" />
       </ListItem>
-      <Dialog isVisible={visible}>
-        <Text>Are you sure you want to log out?</Text>
+      <Dialog testID="dialog-confirm-logout" isVisible={visible}>
+        {loading ? <Dialog.Loading /> : <Text>Are you sure you want to log out?</Text>}
         <Dialog.Actions>
-          <Dialog.Button onPress={handleYesPress}>Yes</Dialog.Button>
-          <Dialog.Button onPress={handleNoPress}>No</Dialog.Button>
+          <Dialog.Button onPress={handleYesPress} testID="dialog-button-yes">
+            Yes
+          </Dialog.Button>
+          <Dialog.Button onPress={handleNoPress} testID="dialog-button-no">
+            No
+          </Dialog.Button>
         </Dialog.Actions>
       </Dialog>
     </View>
