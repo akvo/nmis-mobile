dd73644e1e5bd7b5de63e1edad5870a52962c62c
ifirmawan <firmawaneiwan@gmail.com>
2023-07-07 17:34:01 +0700

    [#27] Add left component: goToUsers

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from '@rneui/themed';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { BaseLayout } from '../components';
 
 const Home = ({ navigation }) => {
@@ -6,6 +8,10 @@ const Home = ({ navigation }) => {
   const goToManageForm = (id) => {
     const findData = data?.find((d) => d?.id === id);
     navigation.navigate('ManageForm', { id, name: findData?.name });
+  };
+
+  const goToUsers = () => {
+    navigation.navigate('Users');
   };
 
   const data = Array.from({ length: 100 })
@@ -24,6 +30,11 @@ const Home = ({ navigation }) => {
         value: search,
         action: setSearch,
       }}
+      leftComponent={
+        <Button type="clear" testID="button-users" onPress={goToUsers}>
+          <Icon name="person" size={18} />
+        </Button>
+      }
     >
       <BaseLayout.Content data={data} action={goToManageForm} columns={2} />
     </BaseLayout>
