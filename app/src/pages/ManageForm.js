9e87a75f42f63518bb9628ad652c2ae9dfd3a077
ifirmawan <firmawaneiwan@gmail.com>
2023-07-17 10:09:03 +0700

    [#31] Update FormState to store selected form

--- app/src/pages/ManageForm.js
+++ app/src/pages/ManageForm.js
@@ -3,9 +3,13 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import { View } from 'react-native';
 import { ListItem } from '@rneui/themed';
 import { BaseLayout } from '../components';
+import { FormState } from '../store';
 
 const ManageForm = ({ navigation, route }) => {
   const goTo = (page) => {
+    FormState.update((s) => {
+      s.form = route?.params;
+    });
     navigation.navigate(page, { ...route?.params });
   };
 
@@ -40,7 +44,7 @@ const ManageForm = ({ navigation, route }) => {
           }}
         >
           {items.map((i, ix) => (
-            <ListItem key={ix} onPress={() => goTo(i.navigation)}>
+            <ListItem key={ix} onPress={() => goTo(i.navigation)} testID={`goto-item-${ix}`}>
               <Icon name={i.icon} color="grey" size={18} />
               <ListItem.Content>
                 <ListItem.Title>{i.text}</ListItem.Title>
