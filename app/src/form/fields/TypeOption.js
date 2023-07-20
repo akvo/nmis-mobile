21a58027c0ac6019e3a6b70e4cedc47fcfc6d908
ifirmawan <firmawaneiwan@gmail.com>
2023-07-20 18:28:56 +0700

    [#58] Update FieldLabel in TypeOption

--- app/src/form/fields/TypeOption.js
+++ app/src/form/fields/TypeOption.js
@@ -5,14 +5,30 @@ import { styles } from '../styles';
 import { CheckBox } from '@rneui/themed';
 import { Dropdown } from 'react-native-element-dropdown';
 
-const TypeOption = ({ onChange, values, keyform, id, name, option = [] }) => {
+const TypeOption = ({
+  onChange,
+  values,
+  keyform,
+  id,
+  name,
+  option = [],
+  lang,
+  tooltip,
+  translations,
+}) => {
   const isRadioGroup = React.useMemo(() => {
     return option.length <= 3;
   }, [option]);
 
   return (
     <View style={styles.optionContainer}>
-      <FieldLabel keyform={keyform} name={name} />
+      <FieldLabel
+        keyform={keyform}
+        name={name}
+        lang={lang}
+        tooltip={tooltip}
+        translations={translations}
+      />
       {isRadioGroup ? (
         option.map((opt, opti) => (
           <CheckBox
