aa73e5622b42dfbd8d31c91b787dc3a933476a79
wayangalihpratama <wgprtm@gmail.com>
2023-07-20 15:24:55 +0800

    [#56] Init test case

--- app/src/database/crud/__tests__/crud-datapoints.test.js
+++ app/src/database/crud/__tests__/crud-datapoints.test.js
@@ -1,13 +1,22 @@
 import crudDataPoints from '../crud-datapoints';
+jest.mock('expo-sqlite')
 
-test.todo('selectDataPointById should return the correct data point when given a valid ID');
+describe('crudDataPoints function', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  })
 
-test.todo('selectDataPointById should return an empty array when given an invalid ID');
+  test('selectDataPointById should return the correct data point when given a valid ID', () => {
+    return new Error();
+  });
 
-test.todo('selectSubmittedDatapoints should return the correct list of submitted data points');
+  test.todo('selectDataPointById should return an empty array when given an invalid ID');
 
-test.todo('selectSavedDatapoints should return the correct list of saved data points');
+  test.todo('selectSubmittedDatapoints should return the correct list of submitted data points');
 
-test.todo('saveDataPoint should save the data point to the database correctly');
+  test.todo('selectSavedDatapoints should return the correct list of saved data points');
 
-test.todo('updateDataPoint should update the data point in the database correctly');
+  test.todo('saveDataPoint should save the data point to the database correctly');
+
+  test.todo('updateDataPoint should update the data point in the database correctly');
+})
