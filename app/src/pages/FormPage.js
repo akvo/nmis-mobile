e6d8b7f0736e07af7b89806cb19d57d38cc59a8c
wayangalihpratama <wgprtm@gmail.com>
2023-07-20 19:14:25 +0800

    [#56] Handle submit survey, saved to datapoints table

--- app/src/pages/FormPage.js
+++ app/src/pages/FormPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Platform, ToastAndroid } from 'react-native';
 import { FormContainer } from '../form';
 import { BaseLayout } from '../components';
 import { FormState } from '../store';
+import { crudDataPoints } from '../database/crud';
+import { UserState } from '../store';
 
 const FormPage = ({ navigation, route }) => {
   const selectedForm = FormState.useState((s) => s.form);
+  const userId = UserState.useState((s) => s.id);
 
   const formJSON = React.useMemo(() => {
     if (!selectedForm?.json) {
@@ -17,8 +21,30 @@ const FormPage = ({ navigation, route }) => {
     navigation.navigate('ManageForm', { ...route?.params });
   };
 
-  const handleOnSubmitForm = (values, refreshForm) => {
-    console.log(values, refreshForm);
+  const handleOnSubmitForm = async (values, refreshForm) => {
+    try {
+      const submitData = {
+        form: selectedForm.id,
+        user: userId,
+        name: values.name,
+        submitted: 1,
+        duration: 0, // TODO:: set duration
+        json: values.answers,
+      };
+      await crudDataPoints.saveDataPoint(submitData);
+      if (Platform.OS === 'android') {
+        ToastAndroid.show(`Data point ${values.name} submitted`, ToastAndroid.LONG);
+      }
+      setTimeout(() => {
+        refreshForm();
+        navigation.navigate('FormData', { ...route?.params, showSubmitted: true });
+      }, 100);
+    } catch (err) {
+      console.error(err);
+      if (Platform.OS === 'android') {
+        ToastAndroid.show(`Error! Can't submit data point ${values.name}`, ToastAndroid.LONG);
+      }
+    }
   };
 
   return (
