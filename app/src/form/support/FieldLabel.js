869a5f73cfd519378c0728a7e6a9a9de502d8075
ifirmawan <firmawaneiwan@gmail.com>
2023-07-20 18:24:40 +0700

    [#58] Update FieldLabel to support trans & tooltip

--- app/src/form/support/FieldLabel.js
+++ app/src/form/support/FieldLabel.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import { Text } from '@rneui/themed';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Text, Tooltip } from '@rneui/themed';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { styles } from '../styles';
 
-const FieldLabel = ({ keyform, name = '' }) => {
+const ControlledTooltip = ({ children, popover }) => {
+  const [open, setOpen] = useState(false);
   return (
-    <Text style={styles.fieldLabel} testID="field-label">
-      {!isNaN(keyform) ? `${keyform + 1}. ${name}` : name}
-    </Text>
+    <Tooltip
+      visible={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      popover={popover}
+      backgroundColor="#e5e5e5"
+    >
+      {children}
+    </Tooltip>
+  );
+};
+
+const FieldLabel = ({ keyform = 0, lang = 'en', name, tooltip, translations }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const getTrans = (trans, target) => trans.find((t) => t?.language === target);
+
+  let text = name;
+  if (translations?.length) {
+    const findTransText = getTrans(translations, lang);
+    text = findTransText?.text || text;
+  }
+  const prefix = `${keyform + 1}. `;
+  const labelText = prefix + text;
+
+  let tooltipText = tooltip?.text;
+  if (tooltip?.translations?.length) {
+    const findTransTooltip = getTrans(tooltip.translations, lang);
+    tooltipText = findTransTooltip?.text || tooltipText;
+  }
+
+  return (
+    <View style={styles.fieldLabel}>
+      <Text testID="field-label">{labelText}</Text>
+      {tooltip && (
+        <ControlledTooltip popover={<Text testID="field-tooltip-text">{tooltipText}</Text>}>
+          <Icon name="help-circle" size={18} testID="field-tooltip-icon" />
+        </ControlledTooltip>
+      )}
+    </View>
   );
 };
 
