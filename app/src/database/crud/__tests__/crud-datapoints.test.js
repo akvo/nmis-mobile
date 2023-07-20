707ad7ff49e6337201dea9e0362431da8b84be5e
wayangalihpratama <wgprtm@gmail.com>
2023-07-20 15:19:58 +0800

    [#56] Create todo for crud-datapoints function

---
+++ app/src/database/crud/__tests__/crud-datapoints.test.js
@@ -0,0 +1,13 @@
+import crudDataPoints from '../crud-datapoints';
+
+test.todo('selectDataPointById should return the correct data point when given a valid ID');
+
+test.todo('selectDataPointById should return an empty array when given an invalid ID');
+
+test.todo('selectSubmittedDatapoints should return the correct list of submitted data points');
+
+test.todo('selectSavedDatapoints should return the correct list of saved data points');
+
+test.todo('saveDataPoint should save the data point to the database correctly');
+
+test.todo('updateDataPoint should update the data point in the database correctly');
