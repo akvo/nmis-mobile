691de274465d90d295bfdb0d481e0121b47102e5
wayangalihpratama <wgprtm@gmail.com>
2023-07-21 14:17:06 +0800

    [#64] Create selectUserById query

--- app/src/database/crud/__tests__/crud-users.test.js
+++ app/src/database/crud/__tests__/crud-users.test.js
@@ -1,30 +1,86 @@
 import crudUsers from '../crud-users';
 jest.mock('expo-sqlite');
+import { conn } from '../../conn';
+
+const db = conn.init;
+
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    password: 'password',
+    active: 1,
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    password: 'password',
+    active: 0,
+  },
+];
 
 describe('crudUsers function', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  describe('selectUsers', () => {
-    it('should return false if users does not exist', async () => {
+  describe('getActiveUser', () => {
+    it('should return false if active user does not exist', async () => {
+      const mockData = [];
+      const mockSelectSql = jest.fn((query, params, successCallback) => {
+        successCallback(null, { rows: { length: mockData.length, _array: mockData } });
+      });
+      db.transaction.mockImplementation((transactionFunction) => {
+        transactionFunction({
+          executeSql: mockSelectSql,
+        });
+      });
       const result = await crudUsers.getActiveUser();
       expect(result).toBe(false);
     });
 
-    it('should return users', async () => {
-      // Mock the result set for select
-      const users = [
-        {
-          id: 1,
-          name: 'John Doe',
-          password: 'password',
-        },
-      ];
-      const mockSelectSql = jest.fn(() => users[0]);
-      crudUsers.getActiveUser = mockSelectSql;
+    it('should return active user', async () => {
+      const mockData = users.filter((u) => u.active);
+      const mockSelectSql = jest.fn((query, params, successCallback) => {
+        successCallback(null, { rows: { length: mockData.length, _array: mockData } });
+      });
+      db.transaction.mockImplementation((transactionFunction) => {
+        transactionFunction({
+          executeSql: mockSelectSql,
+        });
+      });
       const result = await crudUsers.getActiveUser();
-      expect(result).toEqual(users[0]);
+      expect(result).toEqual(mockData[0]);
+    });
+  });
+
+  describe('selectUserById', () => {
+    it('should return {} if user with given id not found', async () => {
+      const mockData = [];
+      const mockSelectSql = jest.fn((query, params, successCallback) => {
+        successCallback(null, { rows: { length: mockData.length, _array: mockData } });
+      });
+      db.transaction.mockImplementation((transactionFunction) => {
+        transactionFunction({
+          executeSql: mockSelectSql,
+        });
+      });
+      const result = await crudUsers.selectUserById({ id: 1 });
+      expect(result).toEqual({});
+    });
+
+    it('should return user if user with given id found', async () => {
+      const mockData = users.filter((u) => u.id === 1);
+      const mockSelectSql = jest.fn((query, params, successCallback) => {
+        successCallback(null, { rows: { length: mockData.length, _array: mockData } });
+      });
+      db.transaction.mockImplementation((transactionFunction) => {
+        transactionFunction({
+          executeSql: mockSelectSql,
+        });
+      });
+      const result = await crudUsers.selectUserById({ id: 1 });
+      expect(result).toEqual(mockData[0]);
     });
   });
 });
