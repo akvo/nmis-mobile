31f1282cc04261b79bce80e27e77db4c77dd9863
ifirmawan <firmawaneiwan@gmail.com>
2023-07-31 10:10:19 +0700

    [#42] Set serverUrl in GetStarted test

--- app/src/pages/__tests__/GetStarted.test.js
+++ app/src/pages/__tests__/GetStarted.test.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import renderer from 'react-test-renderer';
+import { renderHook, act } from '@testing-library/react-native';
 import GetStartedPage from '../GetStarted';
 
 describe('GetStartedPage', () => {
+  beforeAll(() => {
+    const { result } = renderHook(() => useState({}));
+    const [currentConfig, setCurrentConfig] = result.current;
+
+    act(() => {
+      setCurrentConfig({
+        serverURL: 'http://127.0.0.1:8080',
+      });
+    });
+  });
+
   test('renders correctly', () => {
     const tree = renderer.create(<GetStartedPage />).toJSON();
     expect(tree).toMatchSnapshot();
