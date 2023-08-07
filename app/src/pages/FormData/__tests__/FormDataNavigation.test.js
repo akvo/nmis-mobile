bb4ce84b0b31bf55cb9ea57a1d710d8873d0ce05
ifirmawan <firmawaneiwan@gmail.com>
2023-08-07 08:10:43 +0700

    [#115] Remove mock component in FormDataNavigation test

--- app/src/pages/FormData/__tests__/FormDataNavigation.test.js
+++ app/src/pages/FormData/__tests__/FormDataNavigation.test.js
@@ -1,36 +1,6 @@
 import React, { useState } from 'react';
-import { render, renderHook, fireEvent, act, waitFor } from '@testing-library/react-native';
+import { render, renderHook, fireEvent } from '@testing-library/react-native';
 import FormDataNavigation from '../FormDataNavigation';
-
-const mockFormDataNavigation = jest.fn();
-
-jest.mock('../FormDataNavigation', () => ({ totalPage, currentPage, setCurrentPage }) => {
-  mockFormDataNavigation(totalPage, currentPage, setCurrentPage);
-
-  const goBack = () => {
-    setCurrentPage(currentPage - 1);
-  };
-  const goNext = () => {
-    setCurrentPage(currentPage + 1);
-  };
-
-  const disabledBack = currentPage === 0;
-  const disabledNext = currentPage === totalPage - 1;
-
-  return (
-    <mock-View>
-      <mock-Button testID="button-back" disabled={disabledBack} onPress={goBack}>
-        Back
-      </mock-Button>
-      <mock-Text testID="text-pagination">
-        {currentPage + 1}/{totalPage}
-      </mock-Text>
-      <mock-Button testID="button-next" disabled={disabledNext} onPress={goNext}>
-        Next
-      </mock-Button>
-    </mock-View>
-  );
-});
 
 describe('FormDataNavigation', () => {
   it('should render correctly', () => {
@@ -67,7 +37,7 @@ describe('FormDataNavigation', () => {
     );
     const btnBack = getByTestId('button-back');
     expect(btnBack).toBeDefined();
-    expect(btnBack.props.disabled).toBeTruthy();
+    expect(btnBack.props.accessibilityState.disabled).toBeTruthy();
   });
 
   it('should next button enabled when currentPage < totalPage - 1', () => {
@@ -231,7 +201,7 @@ describe('FormDataNavigation', () => {
       />,
     );
     // currentPage = 5
-    expect(btnNext.props.disabled).toBeTruthy();
+    expect(btnNext.props.accessibilityState.disabled).toBeTruthy();
     const textPagination = getByTestId('text-pagination');
     expect(textPagination.props.children).toEqual([5, '/', 5]);
   });
