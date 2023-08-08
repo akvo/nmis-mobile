6b5a84f3ab26f18a4dae03dd0d94d88c86c87099
ifirmawan <firmawaneiwan@gmail.com>
2023-08-08 15:15:59 +0700

    [#123] Update TypeDate test to implements formik Field

--- app/src/form/fields/__test__/TypeDate.test.js
+++ app/src/form/fields/__test__/TypeDate.test.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+import { render, fireEvent, act, waitFor } from '@testing-library/react-native';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 import TypeDate from '../TypeDate';
+
+jest.mock('formik', () => ({
+  ...jest.requireActual('formik'),
+}));
 
 describe('TypeDate component', () => {
   it('should render the component correctly', () => {
+    const initValues = { dateField: null };
     const { getByText, getByTestId, queryByTestId } = render(
-      <TypeDate onChange={() => jest.fn()} values={{}} id="dateField" name="Date Field" />,
+      <Formik initialValues={initValues} onSubmit={() => {}}>
+        {({ setFieldValue, values }) => (
+          <TypeDate onChange={setFieldValue} values={values} id="dateField" name="Date Field" />
+        )}
+      </Formik>,
     );
 
     expect(getByText('1. Date Field')).toBeDefined();
@@ -18,10 +29,14 @@ describe('TypeDate component', () => {
   });
 
   test('opens the date picker on input field press', () => {
+    const initValues = { dateField: null };
     const { getByTestId } = render(
-      <TypeDate onChange={() => jest.fn()} values={{}} id="dateField" name="Date Field" />,
+      <Formik initialValues={initValues} onSubmit={() => {}}>
+        {({ setFieldValue, values }) => (
+          <TypeDate onChange={setFieldValue} values={values} id="dateField" name="Date Field" />
+        )}
+      </Formik>,
     );
-
     const dateField = getByTestId('type-date');
     fireEvent(dateField, 'pressIn');
 
@@ -30,9 +45,13 @@ describe('TypeDate component', () => {
   });
 
   test('calls the onChange function with the selected date', () => {
-    const onChangeMock = jest.fn();
+    const initValues = { dateField: null };
     const { getByTestId } = render(
-      <TypeDate onChange={onChangeMock} values={{}} id="dateField" name="Date Field" />,
+      <Formik initialValues={initValues} onSubmit={() => {}}>
+        {({ setFieldValue, values }) => (
+          <TypeDate onChange={setFieldValue} values={values} id="dateField" name="Date Field" />
+        )}
+      </Formik>,
     );
 
     const dateField = getByTestId('type-date');
@@ -41,19 +60,19 @@ describe('TypeDate component', () => {
     const dateTimePicker = getByTestId('date-time-picker');
     fireEvent(dateTimePicker, 'change', { nativeEvent: { timestamp: 1624262400000 } });
 
-    expect(onChangeMock).toHaveBeenCalledWith('dateField', new Date(1624262400000));
+    expect(dateField.props.value).toBe('6/21/2021');
   });
 
   test('should display correct initial value', () => {
     const initialValue = new Date(1624262400000);
 
+    const initValues = { dateField: initialValue };
     const { getByTestId } = render(
-      <TypeDate
-        onChange={() => {}}
-        values={{ dateField: initialValue }}
-        id="dateField"
-        name="Date Field"
-      />,
+      <Formik initialValues={initValues} onSubmit={() => {}}>
+        {({ setFieldValue, values }) => (
+          <TypeDate onChange={setFieldValue} values={values} id="dateField" name="Date Field" />
+        )}
+      </Formik>,
     );
 
     const dateField = getByTestId('type-date');
@@ -61,32 +80,119 @@ describe('TypeDate component', () => {
   });
 
   it('should not show required sign if required param is false and requiredSign is not defined', () => {
-    const wrapper = render(<TypeDate id="dateField" name="Date Field" required={false} />);
-    const requiredIcon = wrapper.queryByTestId('field-required-icon');
+    const initValues = { dateField: null };
+    const { queryByTestId } = render(
+      <Formik initialValues={initValues} onSubmit={() => {}}>
+        {({ setFieldValue, values }) => (
+          <TypeDate
+            onChange={setFieldValue}
+            values={values}
+            id="dateField"
+            name="Date Field"
+            required={false}
+          />
+        )}
+      </Formik>,
+    );
+
+    const requiredIcon = queryByTestId('field-required-icon');
     expect(requiredIcon).toBeFalsy();
   });
 
   it('should not show required sign if required param is false but requiredSign is defined', () => {
-    const wrapper = render(
-      <TypeDate id="dateField" name="Date Field" required={false} requiredSign="*" />,
+    const initValues = { dateField: null };
+    const { queryByTestId } = render(
+      <Formik initialValues={initValues} onSubmit={() => {}}>
+        {({ setFieldValue, values }) => (
+          <TypeDate
+            onChange={setFieldValue}
+            values={values}
+            id="dateField"
+            name="Date Field"
+            required={false}
+            requiredSign="*"
+          />
+        )}
+      </Formik>,
     );
-    const requiredIcon = wrapper.queryByTestId('field-required-icon');
+
+    const requiredIcon = queryByTestId('field-required-icon');
     expect(requiredIcon).toBeFalsy();
   });
 
   it('should not show required sign if required param is true and requiredSign defined', () => {
-    const wrapper = render(
-      <TypeDate id="dateField" name="Date Field" required={true} requiredSign="*" />,
+    const initValues = { dateField: null };
+    const { queryByTestId } = render(
+      <Formik initialValues={initValues} onSubmit={() => {}}>
+        {({ setFieldValue, values }) => (
+          <TypeDate
+            onChange={setFieldValue}
+            values={values}
+            id="dateField"
+            name="Date Field"
+            required={true}
+            requiredSign="*"
+          />
+        )}
+      </Formik>,
     );
-    const requiredIcon = wrapper.queryByTestId('field-required-icon');
+
+    const requiredIcon = queryByTestId('field-required-icon');
     expect(requiredIcon).toBeTruthy();
   });
 
   it('should show required sign with custom requiredSign', () => {
-    const wrapper = render(
-      <TypeDate id="dateField" name="Date Field" required={true} requiredSign="**" />,
+    const initValues = { dateField: null };
+    const { getByText } = render(
+      <Formik initialValues={initValues} onSubmit={() => {}}>
+        {({ setFieldValue, values }) => (
+          <TypeDate
+            onChange={setFieldValue}
+            values={values}
+            id="dateField"
+            name="Date Field"
+            required
+            requiredSign="**"
+          />
+        )}
+      </Formik>,
     );
-    const requiredIcon = wrapper.getByText('**');
+
+    const requiredIcon = getByText('**');
     expect(requiredIcon).toBeTruthy();
+  });
+
+  it('should displays error message on validation', async () => {
+    const validationSchema = Yup.object().shape({
+      dateField: Yup.date().required('Date is required'),
+    });
+
+    const initValues = { dateField: null };
+    const { getByTestId, getByText, debug } = render(
+      <Formik
+        initialValues={initValues}
+        onSubmit={() => {}}
+        validationSchema={validationSchema}
+        validateOnChange
+      >
+        {({ setFieldValue, values }) => (
+          <Form testID="mock-formik">
+            <TypeDate
+              onChange={setFieldValue}
+              values={values}
+              id="dateField"
+              name="Date Field"
+              required
+              requiredSign="*"
+            />
+          </Form>
+        )}
+      </Formik>,
+    );
+    const formEl = getByTestId('mock-formik');
+    fireEvent(formEl, 'onSubmit');
+    await waitFor(() => {
+      expect(getByText('Date is required')).toBeDefined();
+    });
   });
 });
