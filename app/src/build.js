307d1631ddc4bfa4048a8190b39e400add704c6c
dedenbangkit <mail@dedenbangkit.com>
2023-09-06 17:30:15 +0700

    [#148] Restore default build params

--- app/src/build.js
+++ app/src/build.js
@@ -2,7 +2,6 @@ import { SERVER_URL } from '@env';
 import build_json from './build.json';
 
 const defaultBuildParams = {
-  authenticationType: ['code_assignment', 'username', 'password'],
   ...build_json,
   serverURL: SERVER_URL,
 };
